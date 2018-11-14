84a24226a9d9857d073485c7a4b78ee29b9dfc90
Ian lapham <ianlapham@gmail.com>
2018-11-13 23:58:39 -0500

    remove lock

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Web3Provider } from "react-web3";
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Web3Provider><App /></Web3Provider>,
@@ -13,4 +12,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
